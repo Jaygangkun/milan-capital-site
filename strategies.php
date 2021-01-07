359a04469ebd27a9abd32784a7583e3bb3e01cab
JayGangkun <jaygangkun@hotmail.com>
2021-01-07 21:55:35 +0800

    Strategies Page

---
+++ strategies.php
@@ -0,0 +1,79 @@
+
+    <?php
+    $header_wrap_style = 'header--white-bk';
+    include('header.php');
+    ?>
+    
+    <div class="strategies-hero-section" style="background-image:url(images/strategies-hero-bk.png)">
+        <div class="container">
+            <h1 class="section-title text-center">A Legacy of Success.<br>Built for a Future of Advancement.</h1>
+        </div>
+    </div>
+
+    <div class="strategies-desc-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7">
+                    <h2 class="strategies-desc-section-text1 section-title">Milan adapted a series of tried and true principles.</h2>
+                </div>
+                <div class="col-lg-5">
+                    <div class="strategies-desc-section-text2 section-desc">One being Milans revolutionary buy-and-build strategy, leading the way to a new future of alternative asset management.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="strategies-logos-section">
+        <div class="container">
+            <div class="strategies-logo-desc-block">
+                <div class="strategies-logo-desc-block-top">
+                    <img class="strategies-logo-desc-block-logo__img" src="images/logo-color-green.png">
+                    <div class="strategies-logo-desc-block-sep-line"></div>
+                    <div class="strategies-logo-desc-block-title">FLAGSHIP</div>
+                </div>
+                <div class="strategies-logo-desc-block-down section-desc text-center">
+                Our investments are anchored by our deployment in capital in more mundane, often overlooked sectors of the market.
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="strategies-logo-desc-block strategies-logo-desc-block--small">
+                        <div class="strategies-logo-desc-block-top">
+                            <img class="strategies-logo-desc-block-logo__img" src="images/logo-color-green.png">
+                            <div class="strategies-logo-desc-block-sep-line"></div>
+                            <div class="strategies-logo-desc-block-title">DISCOVER</div>
+                        </div>
+                        <div class="strategies-logo-desc-block-down section-desc text-center">
+                        Funds are focused on operational software companies.
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="strategies-logo-desc-block strategies-logo-desc-block--small">
+                        <div class="strategies-logo-desc-block-top">
+                            <img class="strategies-logo-desc-block-logo__img" src="images/logo-color-green.png">
+                            <div class="strategies-logo-desc-block-sep-line"></div>
+                            <div class="strategies-logo-desc-block-title">EXPLORE</div>
+                        </div>
+                        <div class="strategies-logo-desc-block-down section-desc text-center">
+                        Funds are focused on automation and technology driven companies.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="strategies-logo-desc-block">
+                <div class="strategies-logo-desc-block-top">
+                    <img class="strategies-logo-desc-block-logo__img" src="images/logo-color-green.png">
+                    <div class="strategies-logo-desc-block-sep-line"></div>
+                    <div class="strategies-logo-desc-block-title">DISTRESSED</div>
+                </div>
+                <div class="strategies-logo-desc-block-down section-desc text-center">
+                Funds are focused on distressed opportunities.
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    include('footer.php')
+    ?>
