e2a134707ebec8d94cfbd241704e9241fbe34fd0
JayGangkun <jaygangkun@hotmail.com>
2021-01-08 09:12:42 +0800

    Why Milan Page

---
+++ why_milan.php
@@ -0,0 +1,91 @@
+
+    <?php
+    $header_wrap_style = 'header--white-bk';
+    include('header.php');
+    ?>
+    
+    <div class="firm-main-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 mx-auto">
+                    <h2 class="section-title-underline"><span class="section-title-underline__text">Why Milan</span></h2>
+                    <div class="section-desc text-center">At Milan, we build long-term careers by investing in you. We seek to create an environment that cultivates your professional development and personal growth, as we believe 
+your success is our success.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="why-practice-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h2 class="section-title">WE PRACTICE WHAT WE PREACH</h2>
+                    <img class="why-practice-arrow-down__img" src="images/icon-arrow-down-white.png">
+                </div>
+                <div class="col-lg-6">
+                    <div class="section-desc">Honesty and integrity are what drive our business, inside and out.
+There’s a common assertion about Milan. You'll hear it from new hires, tenured team members, portfolio partners, and investors. 
+We do what we say we're going to do.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="why-title-desc-section">
+        <div class="container">
+            <div class="why-title-block">
+                <div class="firm-introduction-title">Entrepreneurial Spirit</div>
+                <div class="firm-introduction-desc">Despite having grown from our humble roots, we remain flexible and nimble to get the job done.</div>
+            </div>
+            <div class="why-title-block">
+                <div class="firm-introduction-title">Pride and Satisfaction</div>
+                <div class="firm-introduction-desc">We’ve created some of the most respected brands.</div>
+            </div>
+            <div class="why-title-block">
+                <div class="firm-introduction-title">Learning and Development</div>
+                <div class="firm-introduction-desc">Despite having grown from our humble roots, we remain flexible and nimble to get the job done.</div>
+            </div>
+            <div class="why-title-block">
+                <div class="firm-introduction-title">Entrepreneurial Spirit</div>
+                <div class="firm-introduction-desc">We make an effort to understand your passions and strengths, and we create opportunities to excel, add value, and build meaningful long-term careers.</div>
+            </div>
+            <div class="why-title-block">
+                <div class="firm-introduction-title">Beyond the Benefits</div>
+                <div class="firm-introduction-desc">We offer highly competitive benefits and the necessary flexibility to help support our valued team members in a variety of circumstances. Our team members are our family.</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="why-call-section">
+        <div class="container">
+            <h2 class="why-call-section-title">Simply Put A Place You Can Call Home.</h2>
+        </div>
+    </div>
+
+    <div class="why-diversity-section" style="background-image:url(images/why-milan-diversity-bk.png)">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="why-diversity-col-wrap">
+                        <h4 class="section-sub-title-small">DIVERSITY AND INCLUSION</h4>
+                        <div class="firm-introduction-desc">There's a place for everybody. This just might be the place for you.
+</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="why-desc-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 mx-auto">
+                    <div class="section-desc">We're constantly trying to improve our workplace to provide our people with a collaborative, supportive and respectful environment that increases the participation and contribution of each and every employee. In parallel, we strive to support a diverse workplace and leverage the effects of diversity to achieve a competitive business advantage.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    include('footer.php')
+    ?>
