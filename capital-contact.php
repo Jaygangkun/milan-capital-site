556b5eefd5505e087726103fb945039ed251284f
JayGangkun <jaygangkun@hotmail.com>
2021-01-08 10:14:51 +0800

    Capital Contact Page

---
+++ capital-contact.php
@@ -0,0 +1,73 @@
+
+    <?php
+    $header_wrap_style = 'header--white-bk';
+    include('header.php');
+    ?>
+    
+    <div class="firm-main-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 mx-auto">
+                    <h2 class="section-title-underline"><span class="section-title-underline__text">Make. Contact. </span></h2>
+                    <div class="section-desc text-center">Just let us know what we can do for you, and we'll return the favor.
+For urgent matters, please contact us by phone. Otherwise, please select one of the following subjects to submit your inquiry. We will do our best to respond within one business day.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="contact-form-section">
+        <div class="container">
+            <div class="contact-form-container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="contact-form-input-wrap">
+                            <select>
+                                <option value="">New Business</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="contact-form-input-wrap">
+                            <input type="text" placeholder="Your Name">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="contact-form-input-wrap">
+                            <input type="email" placeholder="Email">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="contact-form-input-wrap">
+                            <input type="text" placeholder="Subject">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="contact-form-input-wrap">
+                            <textarea placeholder="Your Message"> </textarea>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="link link--green contact-form-btn">SEND MESSAGE</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="contact-desc-section">
+        <div class="container">
+            <div class="contact-desc-text">A photo I.D. is required for all individuals visiting our office. Upon arrival, check in at the security desk and they will direct you to our main reception.</div>
+        </div>
+    </div>
+    <?php
+    include('footer.php')
+    ?>
